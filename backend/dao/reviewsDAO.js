7cea7bbba55e4240abc8afe7aad0fe81a8e0b244
Brice Durand <brice.durand2@gmail.com>
2023-03-03 17:36:13 +0100

    Correctif de reviewsDAO

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -20,9 +20,9 @@ export default class ReviewsDAO {
                 user_id: user._id,
                 date: date,
                 review: review,
-                movie_id: ObjectId(movieId)
+                movie_id: new ObjectId(movieId)
             }
-            return await reviews.insertOne(movieId)
+            return await reviews.insertOne(reviewDoc)
         } catch (e) {
             console.error('unable to post review: ${e}')
             return { error: e }
@@ -32,7 +32,7 @@ export default class ReviewsDAO {
     static async updateReview(reviewId, userId, review, date) {
         try {
             const updateResponse = await reviews.updateOne(
-                { _id: ObjectId(reviewId), user_id: userId }, 
+                { _id: new ObjectId(reviewId), user_id: userId }, 
                 { 
                     $set: {
                         date: date,
@@ -49,7 +49,7 @@ export default class ReviewsDAO {
     static async deleteReview(reviewId, userId) {
         try {
             const deleteResponse = await reviews.deleteOne(
-                { _id: ObjectId(reviewId), user_id: userId }
+                { _id: new ObjectId(reviewId), user_id: userId }
             )     
             return deleteResponse
         } catch (e) {
