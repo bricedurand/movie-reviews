5ec3a30d5f52ecb5cfdacb5a4b31ed6b13c568bd
Brice Durand <brice.durand2@gmail.com>
2023-03-06 17:36:21 +0100

    Authentification toute simple

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,16 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
+  const [user, setUser] = React.useState(null)
+
+  async function login(user = null) {
+    setUser(user)
+  }
+
+  async function logout() {
+    setUser(null)
+  }
+
   return (
     <div className="App">
       <Navbar bg='light' expand='lg'>
@@ -19,8 +29,8 @@ function App() {
         <Navbar.Toggle aria-controls='basic-navbar-nav'/>
         <Navbar.Collapse id="basic-navbar=nav">
           <Nav className="mr-auto">
-            <Nav.Link>Movies</Nav.Link>
-            <Nav.Link>Login</Nav.Link>
+            <Nav.Link><Link to="/movies">Movies</Link></Nav.Link>
+            <Nav.Link>{ user ? <a onClick={logout}>Logout</a> : <Link to="/login">Login</Link> }</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
