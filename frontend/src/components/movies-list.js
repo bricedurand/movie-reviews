5b86832fdaadb983a8a0be551eef6cfa703dda6e
Brice <brice.durand2@gmail.com>
2023-03-07 10:21:55 +0100

    Récupération de la liste des films dans le composant

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import MovieDataService from '../services/movies'
 
-function MoviesList() {
-    return(
-        <div className='App'>
-            MoviesList
-        </div>
-    );
+import { Link } from "react-router-dom"
+import Movie from './movie'
+
+const MoviesList = props => {
+    const [movies, setMovies] = useState([])
+    const [searchTitle, setSearchTitle] = useState("")
+    const [searchRating, setSeartchRating] = useState("")
+    const [ratings, setRatings] = useState(["All Ratings"])
+
+    useEffect(() => {
+        retrieveMovies()
+        retrieveRatings()
+    }, [])
+
+    const retrieveMovies = () => {
+        MovieDataService.getAll().then(response => {
+            console.log(response.data)
+            setMovies(response.data.movies)
+        }).catch (e => { console.log(e) })
+    }
+
+    const retrieveRatings = () => {
+        MovieDataService.getRatings().then(response => {
+            console.log(response.data)
+            setRatings(["All Ratings"].concat(response.data))
+        }).catch ( e => { console.log(e) }) 
+    }
 }
 
 export default MoviesList;
