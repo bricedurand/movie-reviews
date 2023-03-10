d4687d17fc1c26a1ed03ba15a6f8378eb4ebf46c
Brice <brice.durand2@gmail.com>
2023-03-10 14:17:26 +0100

    composant film

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import MovieDataService from '../services/movies'
+import { Link } from 'react-router-dom';
+import { Card, Container, Image, Col, Row, Button, Media} from 'react-bootstrap'
 
-function Movie() {
-    return(
-        <div className='App'>
-            Movie
-        </div>
-    );
+const Movie = props => {
+   const [movie, setMovie] = useState({
+      id: null,
+      title: "",
+      rated: "",
+      reviews: []
+   })
+
+   const getMovie = id => {
+      MovieDataService.get(id).then(response =>{
+         setMovie(response.data)
+         console.log(response.data)
+      }).catch(e => {
+         console.log(e)
+      })
+   }
+
+   useEffect(() => {
+      getMovie(props.match.params.id)
+   }, [props.match.params.id])
+
+   return (
+      <div>
+         <Container>
+            <Row>
+               <Col>
+                  <Image src={movie.poster + "/100px250"} fluid/>
+               </Col>
+               <Col>
+                  <Card>
+                     <Card.Header as="h5">{movie.title}</Card.Header>
+                     <Card.Body>
+                        <Card.Text>{movie.plot}</Card.Text>
+                        { props.user && 
+                           <Link to={"/movies/" + props.match.params.id + "/review"}>Add Review</Link>
+                        }
+                     </Card.Body>
+                  </Card>
+                  <br/>
+                  <h2>Reviews</h2>
+               </Col>
+            </Row>
+         </Container>
+      </div>
+   );
 }
 
 export default Movie;
