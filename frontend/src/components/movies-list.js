364726f9b9d99506a3c22a4e20697c80638bbfbe
Brice <brice.durand2@gmail.com>
2023-03-10 14:17:44 +0100

    Ré indentation de movie-list.js

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import MovieDataService from '../services/movies'
 
 import { Link } from "react-router-dom"
@@ -12,119 +12,119 @@ import Movie from './movie'
 import { Form, Button, Col, Row, Container, Card } from 'react-bootstrap'
 
 const MoviesList = props => {
-    const [movies, setMovies] = useState([])
-    const [searchTitle, setSearchTitle] = useState("")
-    const [searchRating, setSearchRating] = useState("")
-    const [ratings, setRatings] = useState(["All Ratings"])
+   const [movies, setMovies] = useState([])
+   const [searchTitle, setSearchTitle] = useState("")
+   const [searchRating, setSearchRating] = useState("")
+   const [ratings, setRatings] = useState(["All Ratings"])
 
-    useEffect(() => {
-        retrieveMovies()
-        retrieveRatings()
-    }, [])
+   useEffect(() => {
+      retrieveMovies()
+      retrieveRatings()
+   }, [])
 
-    const errorHandling =  e => { console.log(e) }
+   const errorHandling = e => { console.log(e) }
 
-    const retrieveMovies = () => {
-        MovieDataService.getAll().then(response => {
-            console.log(response.data)
-            setMovies(response.data.movies)
-        }).catch (errorHandling)
-    }
+   const retrieveMovies = () => {
+      MovieDataService.getAll().then(response => {
+         console.log(response.data)
+         setMovies(response.data.movies)
+      }).catch(errorHandling)
+   }
 
-    const retrieveRatings = () => {
-        MovieDataService.getRatings().then(response => {
-            console.log(response.data)
-            setRatings(["All Ratings"].concat(response.data))
-        }).catch (errorHandling) 
-    }
+   const retrieveRatings = () => {
+      MovieDataService.getRatings().then(response => {
+         console.log(response.data)
+         setRatings(["All Ratings"].concat(response.data))
+      }).catch(errorHandling)
+   }
 
-    const find = (query, by) => {
-        MovieDataService.find(query, by).then(response => {
-            console.log(response.data)
-            setMovies(response.data.movies)
-        }
-        ).catch(errorHandling)
-    }
+   const find = (query, by) => {
+      MovieDataService.find(query, by).then(response => {
+         console.log(response.data)
+         setMovies(response.data.movies)
+      }
+      ).catch(errorHandling)
+   }
 
-    const findByTitle = () => {
-        find(searchTitle, "title")
-    }
+   const findByTitle = () => {
+      find(searchTitle, "title")
+   }
 
-    const findByRating = () => {
-        if (searchRating === "All Ratings") {
-            retrieveMovies()
-        }
-        else {
-            find(searchRating, "rated")
-        }
-    }
+   const findByRating = () => {
+      if (searchRating === "All Ratings") {
+         retrieveMovies()
+      }
+      else {
+         find(searchRating, "rated")
+      }
+   }
 
-    const onChangeSearchTitle = e => {
-        const searchTitle = e.target.value
-        setSearchTitle(searchTitle);
-    }
+   const onChangeSearchTitle = e => {
+      const searchTitle = e.target.value
+      setSearchTitle(searchTitle);
+   }
 
-    const onChangeSearchRating = e => {
-        const searchRating = e.target.value
-        setSearchRating(searchRating);
-    }
+   const onChangeSearchRating = e => {
+      const searchRating = e.target.value
+      setSearchRating(searchRating);
+   }
 
-    return (
-        <div className='App'>
-            <Container>
-                <Form>
-                    <Row>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control 
-                                    type="text"
-                                    placeholder='Search by title'
-                                    value={searchTitle}
-                                    onChange={onChangeSearchTitle} />
-                            </Form.Group>
-                            <Button
-                                variant="primary"
-                                type="button"
-                                onClick={findByTitle} >Search</Button>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control as="select" onChange={onChangeSearchRating}>
-                                    {ratings.map(rating => {
-                                        return(
-                                            <option value={rating}>{rating}</option>
-                                        )
-                                    })}
-                                </Form.Control>
-                            </Form.Group>
-                            <Button 
-                                variant="primary" 
-                                type="button"
-                                onClick={findByRating}>Search</Button>
-                        </Col>
-                    </Row>
-                </Form>
+   return (
+      <div className='App'>
+         <Container>
+            <Form>
+               <Row>
+                  <Col>
+                     <Form.Group>
+                        <Form.Control
+                           type="text"
+                           placeholder='Search by title'
+                           value={searchTitle}
+                           onChange={onChangeSearchTitle} />
+                     </Form.Group>
+                     <Button
+                        variant="primary"
+                        type="button"
+                        onClick={findByTitle} >Search</Button>
+                  </Col>
+                  <Col>
+                     <Form.Group>
+                        <Form.Control as="select" onChange={onChangeSearchRating}>
+                           {ratings.map(rating => {
+                              return (
+                                 <option value={rating}>{rating}</option>
+                              )
+                           })}
+                        </Form.Control>
+                     </Form.Group>
+                     <Button
+                        variant="primary"
+                        type="button"
+                        onClick={findByRating}>Search</Button>
+                  </Col>
+               </Row>
+            </Form>
 
-                <Row>
-                    { movies.map((movie) => {
-                        return (
-                            <Col>
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img src={ movie.poster + "/100px180"} />
-                                    <Card.Body>
-                                        <Card.Title>{movie.title}</Card.Title>
-                                        <Card.Text>Rating : {movie.rated}</Card.Text>
-                                        <Card.Text>{movie.plot}</Card.Text>
-                                        <Link to={"/movies/" + movie._id}>View Reviews</Link>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )
-                    }) }
-                </Row>
-            </Container>
-        </div>
-    )
+            <Row>
+               {movies.map((movie) => {
+                  return (
+                     <Col>
+                        <Card style={{ width: '18rem' }}>
+                           <Card.Img src={movie.poster + "/100px180"} />
+                           <Card.Body>
+                              <Card.Title>{movie.title}</Card.Title>
+                              <Card.Text>Rating : {movie.rated}</Card.Text>
+                              <Card.Text>{movie.plot}</Card.Text>
+                              <Link to={"/movies/" + movie._id}>View Reviews</Link>
+                           </Card.Body>
+                        </Card>
+                     </Col>
+                  )
+               })}
+            </Row>
+         </Container>
+      </div>
+   )
 }
 
 export default MoviesList;
