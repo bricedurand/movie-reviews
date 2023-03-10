88591d8dbce097f9864e59fab8d3b25247cb086d
Brice <brice.durand2@gmail.com>
2023-03-10 15:29:35 +0100

    Correctif : utilisation des backticks plutôt que quotes

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 
 class MovieDataService {
    getAll(page = 0) {
-      return axios.get('http://localhost:5000/api/v1/movies?page=${page}')
+      return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`)
    }
 
    get(id) {
-      return axios.get('http://localhost:5000/api/v1/movies/id/${id}')
+      return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`)
    }
 
-   find(query, by = "title", page = 0) {
-      return axios.get('http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}')
+   find(query, by = `title`, page = 0) {
+      return axios.get(`http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`)
    }
 
    createReview(data) {
